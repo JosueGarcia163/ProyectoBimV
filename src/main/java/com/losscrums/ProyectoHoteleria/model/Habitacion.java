2ff1ab1b0581481dbed769c819514657acaef0a3
Z-AnD-2023269 <alopez-2023299@kinal.edu.gt>
2024-09-08 16:31:10 -0600

    feat): Se completo la clase modelo habititacion

--- src/main/java/com/losscrums/ProyectoHoteleria/model/Habitacion.java
+++ src/main/java/com/losscrums/ProyectoHoteleria/model/Habitacion.java
@@ -1,5 +1,29 @@
 package com.losscrums.ProyectoHoteleria.model;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Data
 public class Habitacion {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String room;
+    @NotBlank
+    private String capacidad;
+    @NotBlank
+    private Boolean disponibilidad;
+    
+    @NotBlank
+    private Date fechaDisponibilidad;
 }
