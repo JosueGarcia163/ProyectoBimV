c74b5f20fffb41ba318bb83a80a8808452826611
Z-AnD-2023269 <alopez-2023299@kinal.edu.gt>
2024-09-08 19:52:21 -0600

    feat):Se completo la interface HabitacionRepository

--- src/main/java/com/losscrums/ProyectoHoteleria/repository/HabitacionRepository.java
+++ src/main/java/com/losscrums/ProyectoHoteleria/repository/HabitacionRepository.java
@@ -1,5 +1,18 @@
 package com.losscrums.ProyectoHoteleria.repository;
 
-public interface HabitacionRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.losscrums.ProyectoHoteleria.model.Habitacion;
+
+@Repository
+public interface HabitacionRepository extends JpaRepository<Habitacion, Long> {
+
+    List<Habitacion> findByRoomType(String roomType);
+
+    List<Habitacion> findByAvailability(Boolean availability);
+
+    List<Habitacion> findByCapacity(String capacity);
 }
