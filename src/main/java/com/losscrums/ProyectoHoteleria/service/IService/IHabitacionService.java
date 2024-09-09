97dcdc765ac829aa818f6578d83b17d65ae62cac
Z-AnD-2023269 <alopez-2023299@kinal.edu.gt>
2024-09-08 20:03:34 -0600

    feat):Se completo la interface IHabitacionService

--- src/main/java/com/losscrums/ProyectoHoteleria/service/IService/IHabitacionService.java
+++ src/main/java/com/losscrums/ProyectoHoteleria/service/IService/IHabitacionService.java
@@ -1,5 +1,23 @@
 package com.losscrums.ProyectoHoteleria.service.IService;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.losscrums.ProyectoHoteleria.model.Habitacion;
+
 public interface IHabitacionService {
 
+    List<Habitacion> getAllRooms();
+
+    Optional<Habitacion> getRoomById(Long id);
+
+    Habitacion saveRoom(Habitacion habitacion);
+
+    public void deleteRoom(Long id);
+
+    List<Habitacion> findByRoomType(String roomType);
+
+    List<Habitacion> findAvailable(Boolean availability);
+
+    List<Habitacion> findByCapacity(String capacity);
 }
