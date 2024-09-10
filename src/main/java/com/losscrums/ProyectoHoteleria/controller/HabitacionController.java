0c0ee412ad02163a487d4d88a8a5a6fd84021e83
Cristian Luna <cluna-2023308@kinal.edu.gt>
2024-09-09 21:08:10 -0600

    feat: se agrego la funcion de listar habitaciones

--- src/main/java/com/losscrums/ProyectoHoteleria/controller/HabitacionController.java
+++ src/main/java/com/losscrums/ProyectoHoteleria/controller/HabitacionController.java
@@ -1,5 +1,42 @@
 package com.losscrums.ProyectoHoteleria.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.CannotCreateTransactionException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.losscrums.ProyectoHoteleria.service.HabitacionService;
+
+@RestController
+@RequestMapping("/hoteleria/v1")    
 public class HabitacionController {
 
+    @Autowired
+    HabitacionService habitacionService;
+
+    @GetMapping()
+    public ResponseEntity<?> getMethodName() {
+        Map<String, Object> res = new HashMap<>();
+        try{
+            return ResponseEntity.ok().body(habitacionService.listRoom());
+        } catch (CannotCreateTransactionException err) {
+            res.put("Message", "Error al momento de conectarse a la db");
+            res.put("Error", err.getMessage().concat(err.getMostSpecificCause().getMessage()));
+            return ResponseEntity.status(503).body(res);    
+        } catch (DataAccessException err) {
+            res.put("Message", "Error al momento de consultar a la db");
+            res.put("Error", err.getMessage().concat(err.getMostSpecificCause().getMessage()));
+            return ResponseEntity.status(503).body(res);
+        } catch (Exception err) {
+            res.put("Message", "Error general al obtener datos");
+            res.put("Error", err.getMessage());
+            return ResponseEntity.internalServerError().body(res);
+        }
+    }
 }
