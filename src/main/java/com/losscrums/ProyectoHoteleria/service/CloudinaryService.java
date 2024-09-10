43e4c8233a4c7283eb021239ea201835021a8cd5
palacios <hpalacios-2020586@kinal.edu.gt>
2024-09-09 22:05:35 -0600

    ducs: se agrego documentacion a la clase cloudinary service

--- src/main/java/com/losscrums/ProyectoHoteleria/service/CloudinaryService.java
+++ src/main/java/com/losscrums/ProyectoHoteleria/service/CloudinaryService.java
@@ -1,29 +1,38 @@
-package main.java.com.harlinpalacios.futecaManager.service;
+package com.losscrums.ProyectoHoteleria.service;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
-import com.harlinpalacios.futecaManager.configs.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+//Se agrego la anotacion @Service para la logica de negocio
 @Service
 public class CloudinaryService {
 
-    @AutoWired 
+    //La anotacion @Autowired nos proporciona un control al momento de inyectar dependencias o instancias en el contexto String
+    @Autowired
     private Cloudinary cloudinary;
-    public Map<String, Object> uploadProfilePinture(MultiparFile file, String folder) throws IOException{
+    public Map<String, Object> uploadProfilePinture(MultipartFile file, String filder) throws IOException{
             String originalFilename = file.getOriginalFilename();
     
         if(originalFilename == null || originalFilename.isEmpty()){
             throw new IllegalArgumentException("El archivo no puede ser nulo o estar vacio");
         }else{
-            //Desfragmentar el nombre y extencion para crear un archivo con nombre nuevo
+            //Se desfragmenta el nombre y extencion para crear un documento con un nuevo nombre
             String newName = originalFilename.substring(0,originalFilename.lastIndexOf('.'));
-            String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+            String timestamp = new SimpleDateFormat("yyyy").format(new Date());
             String finalName = newName + "_" + timestamp;
 
             @SuppressWarnings("unchecked")
             Map<String, Object> uploadResult = (Map<String, Object>) cloudinary.uploader().upload(file.getBytes(),ObjectUtils.asMap(
-                "folder", folder,
+                "folder" , folder,
                 "public_id", finalName
             ));
     }
