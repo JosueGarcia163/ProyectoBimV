63b1995bf714fcfe2109f3fc505f4207a85eb002
Z-AnD-2023269 <alopez-2023299@kinal.edu.gt>
2024-09-09 20:51:52 -0600

    Refactor): Se modifico la interface HabitacionRepository

--- src/main/java/com/losscrums/ProyectoHoteleria/repository/HabitacionRepository.java
+++ src/main/java/com/losscrums/ProyectoHoteleria/repository/HabitacionRepository.java
@@ -3,11 +3,9 @@ package com.losscrums.ProyectoHoteleria.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.losscrums.ProyectoHoteleria.model.Habitacion;
 
-@Repository
 public interface HabitacionRepository extends JpaRepository<Habitacion, Long> {
 
     List<Habitacion> findByRoomType(String roomType);
