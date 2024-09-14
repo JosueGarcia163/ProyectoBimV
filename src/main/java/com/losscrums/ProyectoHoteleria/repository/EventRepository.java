46dd53669894d1d315414a22cde1ac5bdc99431f
amonroy-2023021 <amonroy-2023021@kinal.edu.gt>
2024-09-13 22:26:14 -0600

    add: Se agrego la clase EventRepository y se hace un metodo que liste todos los eventos de un hotel

---
+++ src/main/java/com/losscrums/ProyectoHoteleria/repository/EventRepository.java
@@ -0,0 +1,14 @@
+package com.losscrums.ProyectoHoteleria.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.losscrums.ProyectoHoteleria.model.Event;
+import com.losscrums.ProyectoHoteleria.model.Hotel;
+
+public interface EventRepository extends JpaRepository<Event, Long>{
+    List<Event> findByHote (Hotel hotel);
+
+
+}
