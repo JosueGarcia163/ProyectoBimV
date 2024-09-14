737e81b8d1f658cba0444473a80ac2c6347773a2
amonroy-2023021 <amonroy-2023021@kinal.edu.gt>
2024-09-13 22:38:58 -0600

    add: Se agrego la clase EventService y se crearon los metodos necesarios

---
+++ src/main/java/com/losscrums/ProyectoHoteleria/service/EventService.java
@@ -0,0 +1,38 @@
+package com.losscrums.ProyectoHoteleria.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.losscrums.ProyectoHoteleria.model.Event;
+import com.losscrums.ProyectoHoteleria.repository.EventRepository;
+
+@Service
+public class EventService implements IEventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Override
+    public List<Event> listarEventos() {
+        return eventRepository.findAll();
+    }
+
+    @Override
+    public Event buscarEvento(Long idEvent) {
+        // Buscar evento por ID, si no se encuentra retorna null
+        return eventRepository.findById(idEvent).orElse(null);
+    }
+
+    @Override
+    public Event guardarEvento(Event event) {
+        // Guardar o actualizar un evento
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public void eliminarEvento(Event event) {
+        // Eliminar un evento
+        eventRepository.delete(event);}
+}
