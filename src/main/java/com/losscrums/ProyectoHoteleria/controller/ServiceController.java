2c8a1114880dd36da9cb9d29a5de3f999d40dc07
Cristian Luna <cluna-2023308@kinal.edu.gt>
2024-09-15 14:08:52 -0600

    add: se agrego el metodo de eliminar servicio en el controller

--- src/main/java/com/losscrums/ProyectoHoteleria/controller/ServiceController.java
+++ src/main/java/com/losscrums/ProyectoHoteleria/controller/ServiceController.java
@@ -7,15 +7,16 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.CannotCreateTransactionException;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.losscrums.ProyectoHoteleria.DTO.ServiceResponseDTO;
@@ -146,5 +147,27 @@ public class ServiceController {
             }
         }
     
-    
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Map<String, Object>> deleteService(
+        @PathVariable long id) {
+            Map<String, Object> res = new HashMap<>();
+            try{
+                Services existingServices = servicesService.findService(id);
+
+                if(existingServices == null){
+                    res.put("message", "No se encontro el servicio co la identificacion proporcionada");
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
+                }
+
+                servicesService.deleteService(existingServices);
+                res.put("message", "Servicio eliminado correctamente");
+                res.put("success", true);
+                return ResponseEntity.ok(res);
+            }catch (Exception err){
+                res.put("message", "Error al intentar eliminar el servicio");
+                res.put("error", err.getMessage());
+                res.put("success", false);
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+            }
+        }
 }
